f4c67923804f7675df5bf5b13b68f395b5689067
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-10-14 09:35:30 +0200

    feat: initialise le projet angular-fake-backend

---
+++ apps/angular-fake-backend/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'rc-root',
+  template: `<h1>Comment se passer d'un backend ✌🏼</h1> `,
+})
+export class AppComponent {}
