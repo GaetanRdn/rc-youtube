e3a4fb191fb01f25884a36e106690ea79af765fc
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-04 10:50:52 +0100

    feat: Add a 404 Page Not Found component
    Introduce a PageNotFoundComponent for handling unknown routes and update routing to redirect to this component when no other route matches. This enhances the user experience by providing a clear message and a link to return to the home page.

--- apps/angular-routing/src/app/app.routes.ts
+++ apps/angular-routing/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { CoachingOneHourComponent } from './coaching-one-hour.component';
 import { ContactComponent } from './contact.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 export const appRoutes: Route[] = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -12,4 +13,5 @@ export const appRoutes: Route[] = [
   },
   { path: 'one-month', component: CoachingOneHourComponent },
   { path: 'contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
---
+++ apps/angular-routing/src/app/page-not-found.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'rc-page-not-found',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `<h1 class="text-3xl">404 Not Found</h1>
+    <a class="inline-flex" routerLink="/home">Retour Accueil</a> `,
+  imports: [RouterLink],
+})
+export class PageNotFoundComponent {}
