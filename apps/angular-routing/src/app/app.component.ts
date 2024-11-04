ca6a797e4b0d8a46320b96cd58f2fcc5ee4400a9
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-04 10:20:37 +0100

    feat: Switch to Angular Router for page navigation
    Replaced manual navigation signaling with Angular's RouterOutlet for managing page transitions. This simplifies the `AppComponent` by removing `WritableSignal` usage and improves routing consistency across the application.

--- apps/angular-routing/src/app/app.component.ts
+++ apps/angular-routing/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { CoachingOneHourComponent } from './coaching-one-hour.component';
 import { CoachingOneMonthComponent } from './coaching-one-month.component';
 import { ContactComponent } from './contact.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -10,32 +11,22 @@ import { ContactComponent } from './contact.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <header class="flex gap-4 items-center cursor-pointer border-2">
-      <a class="w-10 h-10 inline-flex" (click)="currentPage.set('home')">
+      <a class="w-10 h-10 inline-flex">
         <span class="bg-header-logo bg-cover bg-center w-10 h-10"></span>
         <span class="sr-only">lien pour rediriger vers la home page</span>
       </a>
 
       <nav class="flex items-center justify-items-center divide-x-2">
-        <a class="py-2 px-4" (click)="currentPage.set('one-hour')">Coaching 1h</a>
-        <a class="py-2 px-4" (click)="currentPage.set('one-month')">Coaching 1 mois</a>
-        <a class="py-2 px-4" (click)="currentPage.set('contact')">Contact</a>
+        <a class="py-2 px-4">Coaching 1h</a>
+        <a class="py-2 px-4">Coaching 1 mois</a>
+        <a class="py-2 px-4">Contact</a>
       </nav>
     </header>
 
     <main class="w-full h-full p-4 flex content-center justify-center">
-      @switch (currentPage()) { @case ('one-hour') {
-      <rc-coaching-one-hour />
-      } @case ('one-month') {
-      <rc-coaching-one-month />
-      } @case ('contact') {
-      <rc-contact />
-      } @default {
-      <rc-home />
-      } }
+      <router-outlet />
     </main>
   `,
-  imports: [HomeComponent, CoachingOneHourComponent, CoachingOneMonthComponent, ContactComponent],
+  imports: [HomeComponent, CoachingOneHourComponent, CoachingOneMonthComponent, ContactComponent, RouterOutlet],
 })
-export class AppComponent {
-  protected readonly currentPage: WritableSignal<'home' | 'one-hour' | 'one-month' | 'contact'> = signal('home');
-}
+export class AppComponent {}
