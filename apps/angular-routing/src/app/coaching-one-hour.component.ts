5f81c58a46f59b22d438c2280411dc8266390ad9
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-10 16:09:59 +0100

    refacto: adjust CSS classes for responsive layout
    Adjusted the CSS classes in multiple components to ensure a more responsive layout. Removed fixed height and added width adjustments to improve flexibility in different viewports.

--- apps/angular-routing/src/app/coaching-one-hour.component.ts
+++ apps/angular-routing/src/app/coaching-one-hour.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'flex flex-col gap-8 justify-center items-center h-screen',
+    class: 'flex flex-col h-auto w-full',
   },
   template: `<h1 class="text-2xl">Tu cherches un accompagnement d'1 heure ?</h1>`,
 })
--- apps/angular-routing/src/app/coaching.component.ts
+++ apps/angular-routing/src/app/coaching.component.ts
@@ -4,9 +4,11 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'rc-coaching',
   standalone: true,
-  styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterOutlet],
+  host: {
+    class: 'w-full h-auto flex flex-col gap-8 p-8',
+  },
   template: `<h1 class="text-3xl">Mes coachings</h1>
     <router-outlet /> `,
 })
