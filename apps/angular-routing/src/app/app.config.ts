0521da2af54d48438473d2d70ad713768b93e6d4
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-11 23:05:48 +0100

    refacto: add view transitions to app config
    Added view transition animations in CSS and updated app configuration to incorporate these transitions within the Angular router. This enhances UI dynamics when navigating between views.

--- apps/angular-routing/src/app/app.config.ts
+++ apps/angular-routing/src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes, withComponentInputBinding()),
+    provideRouter(appRoutes, withComponentInputBinding(), withViewTransitions()),
   ],
 };
