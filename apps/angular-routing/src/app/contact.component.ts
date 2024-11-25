0d81205fc155487003d9d23d374cc1244fdc317f
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-25 12:49:38 +0100

    refacto: add reactive form for contact component
    Integrated a reactive form with form controls and validation in the contact component. Added a submit handler to facilitate form submission.

--- apps/angular-routing/src/app/contact.component.ts
+++ apps/angular-routing/src/app/contact.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'rc-contact',
@@ -7,6 +8,61 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   host: {
     class: 'flex flex-col gap-8 justify-center items-center h-screen',
   },
-  template: `<h1 class="text-3xl">N'hésite pas à me contacter</h1>`,
+  template: `<h1 class="text-3xl">N'hésite pas à me contacter</h1>
+
+    <form
+      class="flex flex-col gap-4 *:flex *:justify-between *:gap-4"
+      (ngSubmit)="sendContact()"
+      [formGroup]="contactForm"
+    >
+      <fieldset>
+        <label for="name">Nom*</label>
+        <input
+          type="text"
+          name="lastName"
+          id="lastName"
+          class="border border-black rounded"
+          formControlName="lastName"
+        />
+      </fieldset>
+      <fieldset>
+        <label for="name">Gaetan*</label>
+        <input
+          type="text"
+          name="firstName"
+          id="firstName"
+          class="border border-black rounded"
+          formControlName="firstName"
+        />
+      </fieldset>
+      <fieldset>
+        <label for="name">Email*</label>
+        <input type="email" name="email" id="email" class="border border-black rounded" formControlName="email" />
+      </fieldset>
+      <fieldset>
+        <label for="request">Besoin*</label>
+        <textarea name="request" id="request" class="border border-black rounded" formControlName="request"></textarea>
+      </fieldset>
+
+      <button
+        type="submit"
+        class="border border-black rounded-lg w-fit px-4 py-2 enabled:hover:bg-gray-200 disabled:opacity-50"
+        [disabled]="contactForm.invalid"
+      >
+        Je prends contact
+      </button>
+    </form> `,
+  imports: [ReactiveFormsModule],
 })
-export default class ContactComponent {}
+export default class ContactComponent {
+  readonly contactForm: FormGroup = new FormGroup({
+    lastName: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    request: new FormControl('', Validators.required),
+  });
+
+  sendContact() {
+    console.log('send contact');
+  }
+}
