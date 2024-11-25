f50da64d9d3499e0e8170970076e9b0f284ff88e
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-25 11:20:31 +0100

    refacto: remove unnecessary null check in template
    Removed the unnecessary null check in the template binding in `coaching-detail.component.ts`. This simplification assumes the `coaching()` function always returns a non-null value, optimizing the component rendering.

--- apps/angular-routing/src/app/coaching-detail.component.ts
+++ apps/angular-routing/src/app/coaching-detail.component.ts
@@ -5,7 +5,7 @@ import { Coaching } from './infra/coaching-in-memory.service';
   selector: 'rc-coaching-detail',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: ` <h2 class="text-xl">{{ coaching()?.name }}</h2> `,
+  template: ` <h2 class="text-xl">{{ coaching().name }}</h2> `,
 })
 export class CoachingDetailComponent {
   protected readonly coaching: InputSignal<Coaching> = input.required();
