a73e514be615caca5b7b2bd94a60c1df90a70750
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2024-11-25 12:16:01 +0100

    refacto: update input handling and template for EakComponent
    Refactored EakComponent to use InputSignalWithTransform for input handling. Updated the template to include transformed animation value.

--- apps/angular-routing/src/app/eak.component.ts
+++ apps/angular-routing/src/app/eak.component.ts
@@ -1,11 +1,13 @@
-import { Component, input } from '@angular/core';
+import { Component, input, InputSignalWithTransform } from '@angular/core';
 
 @Component({
   selector: 'rc-eak',
   standalone: true,
   host: { class: 'flex flex-col gap-8 justify-center items-center h-screen' },
-  template: ` <h1 class="text-3xl" [class.animate-bounce]="withAnimation()">EAK</h1> `,
+  template: ` <h1 class="text-3xl" [class.animate-bounce]="withAnimation()">EAK {{ withAnimation() + '' }}</h1> `,
 })
 export default class EakComponent {
-  withAnimation = input(true);
+  withAnimation: InputSignalWithTransform<boolean, unknown> = input(true, {
+    transform: (value: unknown) => value !== 'false',
+  });
 }
